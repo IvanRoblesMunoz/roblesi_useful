3e828e3bfe10520d622ce2d6c2988a408f9e33dd
IvanRoblesMunoz <ivan.robles.munoz@hotmail.com>
2020-07-25 17:17:47 +0100

    options in unique columns added

--- roblesi_useful.py
+++ roblesi_useful.py
@@ -3,12 +3,16 @@
 import pandas as pd
 
 
-def eda(data):
+def eda(data, unique=False, unique_ls="all"):
     """
     Returns a review of the column types, number and percentages of Null and unique values
 
     Args:
       data (pandasDataFrame) : data to be evaluated
+      unique (boolean) :  If true return count and percentage of unique values otherwise dont return
+      unique_ls (list or string) :  if 'all' is returned as a string, then return all columns,
+              otherwise pass a list of column names to be evaluated, all other columns will be empty
+
     Returns:
       Columns (pandasDataFrame) : a pandas dataframe with the overview of the data as mentioned
       above, it contains a row per column in the orginal dataframe to evaluate
@@ -26,14 +30,19 @@ def eda(data):
     nulls = nulls.reset_index().rename(columns={"index": "col_name"})
     columns = columns.merge(nulls, left_on="col_name", right_on="col_name", how="outer")
 
-    # Count Number of unique values
-    nunique = pd.DataFrame(
-        data[list(columns["col_name"])].nunique(), columns=["unique_count"]
-    )
+    if unique:
 
-    nunique["unique_count_perc"] = nunique["unique_count"] / len(data) * 100
-    nunique = nunique.reset_index().rename(columns={"index": "col_name"})
-    columns = columns.merge(
-        nunique, left_on="col_name", right_on="col_name", how="outer"
-    )
+        # If all columns are selected, then
+        if unique_ls == "all":
+            unique_ls = data.columns
+
+        # Count Number of unique values
+        nunique = pd.DataFrame(data[unique_ls].nunique(), columns=["unique_count"])
+
+        nunique["unique_count_perc"] = nunique["unique_count"] / len(data) * 100
+        nunique = nunique.reset_index().rename(columns={"index": "col_name"})
+        columns = columns.merge(
+            nunique, left_on="col_name", right_on="col_name", how="outer"
+        )
+
     return columns
